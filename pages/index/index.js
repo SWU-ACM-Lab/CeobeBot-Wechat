161f73cf1c4a57d65c4a8d980686db3fb6db33c3
sunist <sunist.c@outlook.com>
2021-10-15 23:40:11 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,200 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
+// pages/index/index.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    currentTab: "index",
+    currentColor: "#6B8E23",
+    currentThemeId: 2,
+
+    themeColor: [
+      {
+        "id": "pink",
+        "name": "猛男粉",
+        "color": "#FFC0CB",
+      },
+      {
+        "id": "violet",
+        "name": "基佬紫",
+        "color": "#8A2BE2"
+      },
+      {
+        "id": "green",
+        "name": "原谅绿",
+        "color": "#6B8E23"
+      },
+      {
+        "id": "black",
+        "name": "斑斓黑",
+        "color": "#000000"
+      },
+      {
+        "id": "yellow",
+        "name": "内心黄",
+        "color": "#FFD700"
+      }
+    ],
+    
+    tabBarStatus : [
+      {
+        "dot": false,
+      },
+      {
+        "dot": false,
+      },
+      {
+        "dot": false,
+      },
+      {
+        "dot": false,
+      },
+      {
+        "dot": false,
+      }
+    ],
+
+    indexDropdownItems: [
+      {
+        "value": 0,
+        "text": "无前缀",
+        "items": [
+          {
+            "value": 0,
+            "text": "无前缀",
+            "items": [
+              {
+                "value": 0,
+                "text": "无前缀"
+              }
+            ]
+          },
+        ]
+      }
+    ],
+    indexDropdownI: 0,
+    indexDropdownII: 0,
+    indexDropdownIII: 0,
+    indexDropdownEnableII: false,
+    indexDropdownEnableIII: false,
+
+    indexInputValue: "",
+
+    actions: [
+      {
+        name: '选项',
+      },
+      {
+        name: '选项',
+      },
+      {
+        name: '选项',
+        subname: '描述信息',
+        openType: 'share',
+      },
+    ],
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
   },
-  onLoad() {
-    if (wx.getUserProfile) {
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  handleIndexDropdownIChange(res) {
+    this.setData({
+      indexDropdownI: res.detail
+    });
+
+    if (this.data.indexDropdownItems[this.data.indexDropdownI].items.length > 1) {
       this.setData({
-        canIUseGetUserProfile: true
+        indexDropdownEnableII: true,
+        indexDropdownII: 0
+      })
+    } else {
+      this.setData({
+        indexDropdownEnableII: false
+      })
+    }
+
+    if (this.data.indexDropdownItems[this.data.indexDropdownI].items[this.data.indexDropdownII].items.length > 1) {
+      this.setData({
+        indexDropdownEnableIII: true,
+        indexDropdownIII: 0
+      })
+    } else {
+      this.setData({
+        indexDropdownEnableIII: false
       })
     }
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
+
+  handleChangeTheme(res) {
+    this.setData({
+      currentThemeId: res.detail-1,
+      currentColor: this.data.themeColor[res.detail-1].color
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+
+  handleInputChange(res) {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      indexInputValue: res.detail
     })
+  },
+
+  handleMessageSend(res) {
+    console.log(this.data.indexInputValue)
+    console.log(this.data.indexDropdownValue)
   }
 })
